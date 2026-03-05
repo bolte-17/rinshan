f2cf2e4336685982d5fc02bc6d693e8336cd9409
Brian Bolte <3034257+bolte-17@users.noreply.github.com>
2026-03-05 16:59:00 -0500

    Collapse navbar on small screens

--- lib/rinshan_web/components/navs.ex
+++ lib/rinshan_web/components/navs.ex
@@ -7,27 +7,62 @@ defmodule RinshanWeb.Navs do
 
   def navbar(assigns) do
     ~H"""
-    <header class="navbar text-primary xl:px-8">
-      <div class="flex-1">
+    <div class="navbar text-primary bg-base-100 shadow-sm">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabindex="-1"
+            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <.nav_links />
+          </ul>
+        </div>
         <a href="/" class="flex-1 flex w-fit items-center gap-2">
           <img src={~p"/images/logo.svg"} width="36" />
         </a>
       </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-5 items-center">
-          <li><.link navigate="/" class="link link-hover">Home</.link></li>
-          <%!-- <li><.link to="/leaderboard" class="link link-hover">Leaderboard</.link></li> --%>
-          <li :for={page <- RinshanWeb.StaticPages.all_pages()} :if={page.navlink > 0}>
-            <.link navigate={"/#{page.route}"} class="link link-hover capitalize">{page.title}</.link>
-          </li>
-          <li>
-            <Components.social_link social="discord" class="link link-hover">
-              Join Us
-            </Components.social_link>
-          </li>
+      <%!-- <div class="navbar-start hidden lg:flex">
+        <a href="/" class="flex-1 flex w-fit items-center gap-2">
+          <img src={~p"/images/logo.svg"} width="36" />
+        </a>
+      </div> --%>
+      <div class="navbar-center hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <.nav_links />
         </ul>
       </div>
-    </header>
+    </div>
+    """
+  end
+
+  defp nav_links(assigns) do
+    ~H"""
+    <li><.link navigate="/" class="link link-hover">Home</.link></li>
+    <%!-- <li><.link to="/leaderboard" class="link link-hover">Leaderboard</.link></li> --%>
+    <li :for={page <- RinshanWeb.StaticPages.all_pages()} :if={page.navlink > 0}>
+      <.link navigate={"/#{page.route}"} class="link link-hover capitalize">{page.title}</.link>
+    </li>
+    <li>
+      <Components.social_link social="discord" class="link link-hover">
+        Join Us
+      </Components.social_link>
+    </li>
     """
   end
 
